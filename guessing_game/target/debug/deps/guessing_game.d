ea018eb6ef29fe17d733bd6a52d3a9e30d1162b4
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-18 11:14:07 +0800

    delete

--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-D:\lab\learning-rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-D:\lab\learning-rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-D:\lab\learning-rust\guessing_game\target\debug\guessing_game.exe: D:\lab\learning-rust\guessing_game\src\main.rs
