696909c4999234be69ba19039ffdb855e3551ae1
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-19 14:19:30 +0800

    chapter 2 variable and shadowing

---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {}", x);
+    x = 7;
+    println!("The value of x is {}", x);
+    const MAX_POINT: u32 = 100_000;
+    println!("The value of MAX_POINT is {}", MAX_POINT);
+}
