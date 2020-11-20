08f1ceb065942da4352af9054dfa3254560d54b4
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-20 14:08:23 +0800

    for

--- repeating/src/main.rs
+++ repeating/src/main.rs
@@ -15,4 +15,14 @@ fn main() {
         number -= 1;
     }
     println!("LIFTOFF!!");
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
