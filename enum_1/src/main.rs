e418b947433ed386062f5a80a90367a502a9168f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-23 14:20:54 +0800

    enum

---
+++ enum_1/src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    println!("Hello, world! {:?} and {:?}", four, six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    println!("home: {:?}", home);
+    println!("loopback: {:?}", loopback);
+}
