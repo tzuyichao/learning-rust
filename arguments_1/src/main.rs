7e1de55e9160eb6c92f61f3a744299ff8c55f839
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-25 14:26:46 +0800

    command line arguments and reading file

---
+++ arguments_1/src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+}
