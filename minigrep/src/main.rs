a890e770bff031e5c7d379fe7b4e3aaa3af1475b
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-25 15:05:21 +0800

    use struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,15 +3,20 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
     
-    println!("Query: {}", query);
-    println!("In file: {}", filename);
+    println!("Query: {}", config.query);
+    println!("In file: {}", config.filename);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let filename = &args[2];
 
-    (query, filename)
+    Config { query: query.to_string(), filename: filename.to_string() }
 }
