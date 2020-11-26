a6039ef6b75890f817b001ea4dada5cff1dce81f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-26 11:24:23 +0800

    Deref trait first at a glance

---
+++ deref_1/src/main.rs
@@ -0,0 +1,32 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let z = Box::new(x);
+    assert_eq!(5, *z);
+
+    let lambda = MyBox::new(x);
+    assert_eq!(5, *lambda);
+    assert_eq!(5, *(lambda.deref()));
+}
