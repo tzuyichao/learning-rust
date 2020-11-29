7f9a28bf6577e484ea23fe052aec48c954c09a9e
TzuYi Chao <tzuyichao0313@gmail.com>
2020-11-29 16:15:00 +0800

    write html response

--- tcp_hello/src/main.rs
+++ tcp_hello/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,4 +18,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let contents = fs::read_to_string("index.html").unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+    
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
