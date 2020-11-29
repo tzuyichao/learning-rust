2b31ffb39352abef4dcd4afd894e9a0a35d269a6
TzuYi Chao <tzuyichao0313@gmail.com>
2020-11-29 14:26:53 +0800

    function pointer

--- function_pointer_1/src/main.rs
+++ function_pointer_1/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("Hello, world!");
     let answer = do_twice(add_one, 10);
     println!("Answer: {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+    println!("list_of_strings: {:?}", list_of_strings);
 }
 
 fn add_one(x: i32) -> i32 {
