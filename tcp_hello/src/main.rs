3352019e267e3864e6b4c271f3cb4e95eb28e1c7
TzuYi Chao <tzuyichao0313@gmail.com>
2020-11-29 15:56:09 +0800

    handle request

--- tcp_hello/src/main.rs
+++ tcp_hello/src/main.rs
@@ -1,11 +1,19 @@
+use std::io::Read;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
         println!("Connection established!");
+        handle_connection(stream);
     }
-    println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
