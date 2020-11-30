cf1d4c3d4492831ceea70e7d80387219970ac0e6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-30 08:21:56 +0800

    simulate slow operation

--- tcp_hello/src/main.rs
+++ tcp_hello/src/main.rs
@@ -3,6 +3,8 @@ use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -20,8 +22,12 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
