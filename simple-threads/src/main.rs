5579289c476ab2b9c3a18d59cf5076d3174aa622
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-30 11:22:25 +0800

    shared data with thread

--- simple-threads/src/main.rs
+++ simple-threads/src/main.rs
@@ -9,6 +9,12 @@ fn main() {
     }
 
     println!("Waiting for the thread to finish ... {:?}", no_move_thread.join());
+
+    let a_number = 43;
+    let a_vec = vec![1, 2, 3, 4, 5];
+    let move_thread = start_shared_data_thread(a_number, a_vec);
+    println!("Waiting for the thread to finish ... {:?}", move_thread.join());
+    //println!("a_vec: {:?}", a_vec); value borrowed here after move
 }
 
 fn start_no_shared_data_thread() -> thread::JoinHandle<()> {
@@ -16,5 +22,17 @@ fn start_no_shared_data_thread() -> thread::JoinHandle<()> {
         println!("Waiting for three seconds.");
         thread::sleep(Duration::from_secs(3));
         println!("Done.");
+    })
+}
+
+fn start_shared_data_thread(a_number: i32, a_vec: Vec<i32>) -> thread::JoinHandle<Vec<i32>> {
+    thread::spawn(move || {
+        print!(" a_vec ---> [");
+        for i in a_vec.iter() {
+            print!(" {} ", i);
+        }
+        println!("]");
+        println!(" A number from inside the thread: {}", a_number);
+        a_vec
     })
 }
