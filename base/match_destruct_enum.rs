30dddb0f18c0bc43f554626425c39056b25ff288
TERENCE.CHAO <terence.chao@deltaww.com>
2020-12-03 13:56:33 +0800

    enum

---
+++ base/match_destruct_enum.rs
@@ -0,0 +1,26 @@
+enum Color {
+    Red,
+    Blue,
+    Green,
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+fn main() {
+    let color = Color::HSL(122, 17, 40);
+
+    println!("What color is it?");
+    match color {
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {} and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {} and value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {} and lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {} and yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!("Cyan: {}, magenta: {} and yellow: {}, key(black): {}!", c, m, y, k),
+    }
+}
