86814a159f0e30bb06aedfd1a1be40b26391c330
terence <tzuyichao0313@gmail.com>
2023-12-09 20:59:57 +0800

    upgrade

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,21 +1,28 @@
-use actix_web::{web, App, HttpResponse, HttpServer}
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
-    let server =  HttpServer::new(|| {
-        App::new().route("/", web::get().to(get_index))
-    });
-
-    println!("Server is running on http://localhost:3000");
-    server.bind("127.0.0.1:3000").expect("error binding server to address")
-        .run().expect("error running server");
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(
-            r#"
-            <h1>It works</h1>
-            "#,
-        );
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
