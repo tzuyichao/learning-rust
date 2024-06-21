ee3fee359250716213f8919e713a7e3f3f52d4fb
TzuYi Chao <tzuyichao0313@gmail.com>
2024-06-21 06:22:06 +0000

    no panic

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+	process::exit(1);
+    });
     
     println!("Query: {}", config.query);
     println!("In file: {}", config.filename);
@@ -19,12 +23,16 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    if args.len() < 3 {
-        panic!("not enough arguments");
-    }
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
 
-    Config { query, filename }
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+	    return Err("not enough arguments");
+	}
+	let query = args[1].clone();
+	let filename = args[2].clone();
+
+	Ok(Config { query, filename })
+    }
 }
+
