b2f85960026a75e73e9c28c8138e712116989d3e
TzuYi Chao <tzuyichao0313@gmail.com>
2024-06-22 22:11:23 +0800

    chapter 1

---
+++ command-line-rust/chapter1/src/main.rs
@@ -0,0 +1,9 @@
+//use std::process::Command;
+
+fn main() {
+    println!("Hello, world!");
+    // let mut cmd = Command::new("powershell");
+    // cmd.args(&["-Command", "ls"]);
+    // let res = cmd.output();
+    // println!("{:#?}", res);
+}
---
+++ command-line-rust/chapter1/tests/cli.rs
@@ -0,0 +1,12 @@
+use assert_cmd::Command;
+
+#[test]
+fn works() {
+    assert!(true);
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
+}
